ee7118797d2100745b21de704c6f1048513625b4
Dario_DELL <dariomiguel0@gmail.com>
2025-01-08 18:22:53 -0300

    Probando PHP

---
+++ prueba.php
@@ -0,0 +1,35 @@
+<?php
+
+define("curso","php");
+
+// $edad = '1' + '2';
+// echo "hola $edad";
+
+// //print "Hola mundo PHP","hola 2";
+// $edad = 5;
+// echo "hola $edad";
+// echo curso;
+// $curso = "html";
+
+// echo $curso;
+
+$valor1 = 11;
+$valor2 = 2;
+
+// if ($valor1 < $valor2) {
+//     print "El valor $valor1 es menor a $valor2";
+// }else {
+//     echo "No es menor";
+// }
+
+     switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+?>
